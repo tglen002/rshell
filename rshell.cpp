033eef751409358d7b0bebfd3c8c5e3bb4b821ff
Tiffany Glenn-Hall <tglen002@ucr.edu>
2014-10-20 18:32:42 -0700

    execvp working
    need to change code to deal with multiple commands, exit...

--- rshell.cpp
+++ rshell.cpp
@@ -1,5 +1,10 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<errno.h>
 #include<iostream>
 
 
@@ -7,18 +12,50 @@ using namespace std;
 
 int main()
 {
+  char* list[3] = {'\0'};
   string commandLine;
   cout << "$ ";
   getline(cin,commandLine);
-  cout << "output: " << commandLine << endl;
+  //cout << "output: " << commandLine << endl;
   char *charCmmdLine = new char[commandLine.length() + 1];
   strcpy(charCmmdLine, commandLine.c_str());
   char connectors[] = "&|;";
   char * indvCommands;
   indvCommands = strtok(charCmmdLine, connectors);
-  while (indvCommands != NULL)
-    { printf("%s\n", indvCommands); indvCommands = strtok(NULL,connectors);}
+  //if indivCommands = exit then exit the program
+
+  while (indvCommands != NULL){
+    //{ printf("%s\n", indvCommands); indvCommands = strtok(NULL,connectors);}
+    list[0] = strtok(indvCommands," ");
+    list[1] = strtok(NULL," ");
+    pid_t childPID;
+    childPID = fork();
+    if(childPID >= 0){ //fork was successful
+      if(childPID == 0){ //child process
+      //contains execvp
+        //cout << "I am currently in the child" << endl;
+        int r = execvp(list[0], list);
+	if(r == -1){
+	  perror("execvp failed");
+	  exit(1);
+        }
+      }
+      else{ //parent process
+        wait(NULL);
+        //cout << "I am currently in the parent" << endl;
+      }
+    }
+    else{ // fork failed
+      perror("fork failed");
+      exit(1);
+    }
+    indvCommands = strtok(NULL, connectors);
+  }
+
   delete [] charCmmdLine;
+  
+  //char *argv[2]; //need to change in order to fit unlimited number of calls
+  
 
   return 0;
 }
