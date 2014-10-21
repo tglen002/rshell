4e4426d7f69dcffdfd8158fdcb6c794c9db664f5
Tiffany Glenn-Hall <tglen002@ucr.edu>
2014-10-21 13:45:53 -0700

    added ability to complete command line and then take in a new command line

--- rshell.cpp
+++ rshell.cpp
@@ -13,56 +13,62 @@ using namespace std;
 int main()
 {
   char noRun[] = "exit";
-  cout << "$ ";
+  char spaceNoRun[] = " exit";
 
-  string commandLine;
-  getline(cin,commandLine);
-  char *charCmmdLine = new char[commandLine.length() + 1];
-  strcpy(charCmmdLine, commandLine.c_str());
-  char connectors[] = "&|;";
-  char *indvCommands;
-  char *currCmmdLine;
-  indvCommands = strtok_r(charCmmdLine, connectors, &currCmmdLine);
+  while(true){
+    cout << "$ ";
+    fflush(stdout);
+    string commandLine;
+    getline(cin,commandLine);
+    char *charCmmdLine = new char[commandLine.length() + 1];
+    strcpy(charCmmdLine, commandLine.c_str());
+    char connectors[] = "&|;";
+    char *indvCommands;
+    char *currCmmdLine;
+    indvCommands = strtok_r(charCmmdLine, connectors, &currCmmdLine);
 
-  char **list = new char*[commandLine.length() + 1];
-  for(unsigned i = 0; i < commandLine.length() + 1; i++){ list[i] = '\0';}
+    char **list = new char*[commandLine.length() + 1];
+    for(unsigned i = 0; i < commandLine.length() + 1; i++){ list[i] = '\0';}
 
-  while (indvCommands != NULL){
-    if(strcmp(indvCommands,noRun) == 0){
-      exit(EXIT_SUCCESS);
-    }
-    if(true){
-      list[0] = strtok(indvCommands," ");
-      unsigned iterator = 1;
-      while(indvCommands != NULL && iterator < commandLine.length()+1){
-        list[iterator] = strtok(NULL," ");
-        iterator++;
+    while (indvCommands != NULL){
+      if(strcmp(indvCommands,noRun) == 0 || strcmp(indvCommands,spaceNoRun) == 0){
+        exit(EXIT_SUCCESS);
       }
-    }
-
-    pid_t childPID;
-    childPID = fork();
-    if(childPID >= 0){ //fork was successful
-      if(childPID == 0){ //child process
-        int r = execvp(list[0], list);
-        if(r == -1){
-	  perror("execvp failed");
-	  exit(1);
+      if(true){
+        list[0] = strtok(indvCommands," ");
+        unsigned iterator = 1;
+        while(indvCommands != NULL && iterator < commandLine.length()+1){
+          list[iterator] = strtok(NULL," ");
+          iterator++;
         }
       }
-      else{ //parent process
-        wait(NULL);
+
+      pid_t childPID;
+      childPID = fork();
+      if(childPID >= 0){ //fork was successful
+        if(childPID == 0){ //child process
+          int r = execvp(list[0], list);
+          if(r == -1){
+	    perror("execvp failed");
+	    exit(1);
+          }
+        }
+        else{ //parent process
+          wait(NULL);
+        }
       }
+      else{ // fork failed
+        perror("fork failed");
+        exit(1);
+      }
+      indvCommands = strtok_r(NULL, connectors, &currCmmdLine);
+      //cout << "\"" << indvCommands << "\""  << endl;
     }
-    else{ // fork failed
-      perror("fork failed");
-      exit(1);
-    }
-    indvCommands = strtok_r(NULL, connectors, &currCmmdLine);
+    //cout << endl;
+    delete [] charCmmdLine;
+    delete [] list;
   }
 
-  delete [] charCmmdLine;
-  delete [] list;
 
   return 0;
 }
